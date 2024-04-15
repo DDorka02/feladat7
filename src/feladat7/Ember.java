c9e1a6a4b7b5b56d52c841d7c6158f4a4a17bf03
DDorka02 <dobszaydorka@gmail.com>
2024-04-15 10:04:52 +0200

    hatlom

---
+++ src/feladat7/Ember.java
@@ -0,0 +1,10 @@
+package feladat7;
+
+
+public class Ember {
+        int eletkor;
+        String nev, faradt;
+        void Emberek_bemutatása(){
+            System.out.println("Emberek bemutatása:");}
+    }
+
---
+++ src/feladat7/Feladat7.java
@@ -0,0 +1,42 @@
+package feladat7;
+
+public class Feladat7 {
+    
+    
+
+    public static void main(String[] args) {
+        Ember elso = new Ember();
+        elso.nev="Dorka";
+        elso.faradt="Igen";
+        elso.eletkor=21;
+        Ember masodik = new Ember();
+        masodik.nev="András";
+        masodik.faradt="Igen";
+        masodik.eletkor=21;
+        Ember harmadik = new Ember();
+        harmadik.nev="Bori";
+        harmadik.faradt="Nem";
+        harmadik.eletkor=20;
+        Ember negyedik = new Ember();
+        negyedik.nev="Viki";
+        negyedik.faradt="Igen";
+        negyedik.eletkor=21;
+        Ember otodik = new Ember();
+        otodik.nev="Orsi";
+        otodik.faradt="Igen";
+        otodik.eletkor=21;
+        Ember hatodik = new Ember();
+        hatodik.nev="Szella";
+        hatodik.faradt="Igen";
+        hatodik.eletkor=21;
+        Ember hetedik = new Ember();
+        hetedik.nev="Nauzika";
+        hetedik.faradt="Igen";
+        hetedik.eletkor=21;
+        Ember nyolcadik = new Ember();
+        nyolcadik.nev="Dominik";
+        nyolcadik.faradt="Igen";
+        nyolcadik.eletkor=21;
+    }
+   
+}
